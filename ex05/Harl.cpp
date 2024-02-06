028133677599b54981527a59f4865124160e6860
Matthieu De Langhe <mde-lang@k1r4p7.42mulhouse.fr>
2024-02-06 18:39:46 +0100

    ex05

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: mde-lang <mde-lang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 19:54:35 by mde-lang          #+#    #+#             */
-/*   Updated: 2024/02/06 17:56:06 by mde-lang         ###   ########.fr       */
+/*   Updated: 2024/02/06 18:39:08 by mde-lang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,29 +22,30 @@ Harl::~Harl(){
 
 void Harl::complain(std::string level){
 
-
-    Harl::_debug();
-    Harl::_info();
-    Harl::_warning();
-    Harl::_error();
+    void (Harl::*ptr[4])(void) = {&Harl::_debug, &Harl::_info, &Harl::_warning, &Harl::_error};
+    for (int i = 0; i < 4; i++)
+    {
+        if (this->level[i] == level)
+            (this->*ptr[i])();
+    }
 }
 
 void Harl::_debug()
 {
-    std::cout << "love having extra bacon for my 7XL-double-cheese-triple-pickle-special- ketchup burger. I really do!" << std::endl;
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special- ketchup burger. I really do!" << std::endl;
 }
 
 void Harl::_info()
 {
-    std::cout << " cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void Harl::_warning()
 {
-    std::cout << " think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void Harl::_error()
 {
-    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+    std::cout << " This is unacceptable! I want to speak to the manager now." << std::endl;
 }
--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,38 +6,22 @@
 /*   By: mde-lang <mde-lang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 19:54:12 by mde-lang          #+#    #+#             */
-/*   Updated: 2024/02/06 17:54:24 by mde-lang         ###   ########.fr       */
+/*   Updated: 2024/02/06 18:37:40 by mde-lang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int main(){
-
+int main()
+{
     Harl harl;
     std::string input;
-    
-    
 
-    
     while (input != "EXIT")
     {
         std::cin >> input;
-        std::cout << input << std::endl;
-        for (int i = 0; i < 4; ++i) {
-            if (input == harl.level[i]) {
-                switch (i) {
-                    case 0:  = &Harl::_debug break;
-                    case 1:  = &Logger::logInfo; break;
-                    case 2: = &Logger::logWarning; break;
-                    case 3:  = &Logger::logError; break;
-                    default: std::cerr << "Invalid log level!" << std::endl; return;
-            }
-        }
-
-        
+        //std::cout << input << std::endl;
+        harl.complain(input);
     }
-
-
-    
+    return (0);
 }
