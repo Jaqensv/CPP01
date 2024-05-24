681fcf04ca7e108026dcf0d17d96bce7872083df
Matthieu De Langhe <mde-lang@k1r3p2.42mulhouse.fr>
2024-05-24 17:24:48 +0200

    Final

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mde-lang <mde-lang@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/24 17:24:29 by mde-lang          #+#    #+#             */
+/*   Updated: 2024/05/24 17:24:30 by mde-lang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 
 int main(){
@@ -6,11 +18,12 @@ int main(){
     std::string *stringPTR = &str;
     std::string &stringREF = str;
 
-    std::cout << stringPTR << std::endl;
-    std::cout << &str << std::endl;
-    std::cout << &stringREF << std::endl;
-    std::cout << *stringPTR << std::endl;
-    std::cout << str << std::endl;
-    std::cout << stringREF << std::endl;
+    std::cout << "str address: "<< &str << std::endl;
+    std::cout << "stringPTR address: "<< stringPTR << std::endl;
+    std::cout << "stringREF address: "<< &stringREF << std::endl;
+    std::cout << "str value: "<< str << std::endl;
+    std::cout << "stringPTR value: "<< *stringPTR << std::endl;
+    std::cout << "stringREF value: "<< stringREF << std::endl;
+
     return 0;
 }
